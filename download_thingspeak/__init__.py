c34386201ace54108cf9e069ea91016af11d69af
Michael T Smith <lionfishy@gmail.com>
2017-07-12 19:12:11 +0300

    content

---
+++ download_thingspeak/__init__.py
@@ -0,0 +1,52 @@
+import json, requests
+from datetime import datetime, timedelta
+import pickle
+import os
+
+
+def download(apiurl,cache='use'):
+    """
+    download(apiurl,cache='use'):
+
+    Loads thingspeak data from apiurl
+    Set cache to:
+        'use' - to use it
+        'refresh' - to not use it
+        'only' - to only use it
+    """
+    filename = 'channel%s.p'%apiurl.split('/')[-1]
+    cachefile = os.path.isfile(filename)
+    if (cache=='use' or cache=='only') and cachefile:
+        alldata = pickle.load( open( filename, "rb" ) )
+        if (cache=='only'):
+            return alldata
+        nextid = alldata[-1]['entry_id']+1
+        endtime = datetime.strptime(alldata[-1]['created_at'],'%Y-%m-%dT%H:%M:%SZ')+timedelta(seconds=1)
+    else: #no cachefile or refresh -> we want to reload from the API
+        if (cache=='only'):
+            ##TODO Throw exception - can't only use cache as there is no cache
+            assert False, "Can't only use cache as there is no cache"
+        nextid = 1
+        alldata = []
+        endtime = None    
+
+    result = None
+    while result != '-1':
+        result = json.loads(requests.post(apiurl+'/feeds/entry/%d.json' % nextid).content)
+
+        starttime = endtime
+        if result == '-1':
+            endtime = datetime.now()
+        else:
+            endtime = datetime.strptime(result['created_at'],'%Y-%m-%dT%H:%M:%SZ')
+        if (nextid==1):
+            starttime = endtime
+        else:
+            start = datetime.strftime(starttime,'%Y-%m-%dT%H:%M:%SZ')
+            end = datetime.strftime(endtime-timedelta(seconds=1),'%Y-%m-%dT%H:%M:%SZ')
+            data = json.loads(requests.post(apiurl+'/feeds.json?start=%s&end=%s' % (start,end)).content)
+            alldata.extend(data['feeds'])
+        nextid += 7999 #thought download was 8000 fields, but it's 8000 records. 8000/len(result)
+
+    pickle.dump( alldata, open( filename, "wb" ) )
+    return alldata
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'download_thingspeak',
+  packages = ['download_thingspeak'], # this must be the same as the name above
+  version = '1.01',
+  description = 'Download data from a thingspeak channel using their API',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/lionfish0/download_thingspeak.git',
+  download_url = 'https://github.com/lionfish0/download_thingspeak/archive/1.01.tar.gz',
+  keywords = ['thingspeak'],
+  classifiers = [],
+  install_requires=['json','requests','datetime','pickle','os'],
+)
+
