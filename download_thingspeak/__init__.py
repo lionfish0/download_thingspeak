8cb9d97b5ee10a1b5fecaabf9d6c8eba73791b22
Michael T Smith <lionfishy@gmail.com>
2017-07-14 12:17:57 +0300

    add str to date

--- download_thingspeak/__init__.py
+++ download_thingspeak/__init__.py
@@ -4,7 +4,7 @@ import pickle
 import os
 
 
-def download(apiurl,cache='use',verbose=False):
+def download(apiurl,cache='use',verbose=False,apikey=None):
     """
     download(apiurl,cache='use'):
 
@@ -23,7 +23,7 @@ def download(apiurl,cache='use',verbose=False):
             return alldata
         if verbose: print("Using cache")
         nextid = alldata[-1]['entry_id']+1
-        endtime = datetime.strptime(alldata[-1]['created_at'],'%Y-%m-%dT%H:%M:%SZ')+timedelta(seconds=1)
+        endtime = str_to_date(alldata[-1]['created_at'])+timedelta(seconds=1)
     else: #no cachefile or refresh -> we want to reload from the API
         if verbose: print("Ignoring/overwriting cache")
         if (cache=='only'):
@@ -36,22 +36,38 @@ def download(apiurl,cache='use',verbose=False):
     result = None
     if verbose: print("Using %d records from cache" % len(alldata))
     while result != '-1':
-        result = json.loads(requests.post(apiurl+'/feeds/entry/%d.json' % nextid).content)
-
+        #thingspeak doesn't let you download ranges of ids, instead you have to
+        #download ranges of dates. We can only download 8000 at a time, so we
+        #need to get the date of the next one we need (then we ask for that datetime
+        #until now, and repeat until we run out of new items).
+        url = apiurl+'/feeds/entry/%d.json' % (nextid)
+        if apikey is not None: url += '?api_key=%s' % apikey
+        result = json.loads(requests.post(url).content)
         starttime = endtime
+        if result==-1:
+            if verbose: print("Warning: Unable to retrieve data (does channel exist? is it public?)")
         if result == '-1':
             endtime = datetime.now()
         else:
-            endtime = datetime.strptime(result['created_at'],'%Y-%m-%dT%H:%M:%SZ')
+            endtime = str_to_date(result['created_at'])
         if (nextid==1):
             starttime = endtime
         else:
             start = datetime.strftime(starttime,'%Y-%m-%dT%H:%M:%SZ')
             end = datetime.strftime(endtime-timedelta(seconds=1),'%Y-%m-%dT%H:%M:%SZ')
-            data = json.loads(requests.post(apiurl+'/feeds.json?start=%s&end=%s' % (start,end)).content)
-            alldata.extend(data['feeds'])
-            if verbose: print("    Adding %d records..." % len(data['feeds']))
+            url = apiurl+'/feeds.json?start=%s&end=%s' % (start,end)
+            if apikey is not None: url += '&api_key=%s' % apikey            
+            data = json.loads(requests.post(url).content)
+            if (data!=-1):
+                alldata.extend(data['feeds'])
+                if verbose: print("    Adding %d records..." % len(data['feeds']))
+            else:
+                if verbose: print("Warning: unable to read data feed")
+            
         nextid += 7999 #thought download was 8000 fields, but it's 8000 records. 8000/len(result)
     if verbose: print("New cache has %d records, saving." % len(alldata))
     pickle.dump( alldata, open( filename, "wb" ) )
     return alldata
+    
+def str_to_date(st):
+    return datetime.strptime(st,'%Y-%m-%dT%H:%M:%SZ')
